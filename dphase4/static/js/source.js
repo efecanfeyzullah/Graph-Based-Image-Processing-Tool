90ca1f931e334526b4df604a20f649cba6e73099
m-akcay <40623315+m-akcay@users.noreply.github.com>
2023-06-12 01:05:35 +0300

    moving rectangles

--- dphase4/static/js/source.js
+++ dphase4/static/js/source.js
@@ -7,13 +7,21 @@ const buttonMapping = {
     button5: "Title 5",
   };
   
+  let rectangleIdCounter = 0;
   // Function to handle the drag start event
-  function handleDragStart(event) {
+function handleDragStart(event) {
     const target = event.target;
-    event.dataTransfer.setData("text/plain", target.id);
-    target.classList.add("dragging");
-  }
+        event.dataTransfer.setData('text/plain', target.id);
+        target.classList.add('dragging');
+}
   
+function handleRectangleDragStart(event) {
+    const target = event.target;
+    event.dataTransfer.setData('text/plain', 'rectangle');
+    target.classList.add('dragging');
+}
+  
+
   // Function to handle the drag end event
   function handleDragEnd(event) {
     const target = event.target;
@@ -29,22 +37,14 @@ const buttonMapping = {
   function handleDrop(event) {
     event.preventDefault();
   
-    const data = event.dataTransfer.getData("text/plain");
+    const data = event.dataTransfer.getData('text/plain');
     const droppedElement = document.getElementById(data);
-  
-    if (droppedElement) {
-      const dropArea = event.currentTarget;
+        console.log(droppedElement.classList.contains("rectangle"));
 
-  
-      const newRectangle = document.createElement("div");
-      newRectangle.className = "rectangle";
-      const offsetX = event.clientX - 50;
-      const offsetY = event.clientY - 25;
-      newRectangle.textContent = buttonMapping[data];
-      newRectangle.style.left = offsetX + "px";
-      newRectangle.style.top = offsetY + "px";
-  
-      dropArea.appendChild(newRectangle);
+    if (droppedElement) {
+        const dropArea = event.currentTarget;
+        const newRectangle  = createRectangle(event, data);
+        dropArea.appendChild(newRectangle);
     }
   }
   
@@ -54,10 +54,30 @@ const buttonMapping = {
   for (let i = 0; i < menuButtons.length; i++) {
     menuButtons[i].addEventListener("dragstart", handleDragStart);
     menuButtons[i].addEventListener("dragend", handleDragEnd);
+    
   }
-  
+
   // Add event listeners to the drop area
   const dropArea = document.getElementById("drop-area");
   dropArea.addEventListener("dragover", handleDragOver);
   dropArea.addEventListener("drop", handleDrop);
+
+function createRectangle(event, data) {
+    const offsetX = event.clientX - 50;
+    const offsetY = event.clientY - 25;
+
+    const newRectangle = document.createElement('div');
+    newRectangle.className = 'rectangle';
+    newRectangle.textContent = buttonMapping[data];
+    const rectangleId = `rectangle-${rectangleIdCounter}`;
+    newRectangle.setAttribute('id', rectangleId);
+    newRectangle.style.left = offsetX + 'px';
+    newRectangle.style.top = offsetY + 'px';
+    newRectangle.draggable = true;
+    newRectangle.addEventListener('dragstart', handleDragStart);
+    newRectangle.addEventListener('dragend', handleDragEnd);
+
+    rectangleIdCounter++;
+    return newRectangle ;
+}
   
